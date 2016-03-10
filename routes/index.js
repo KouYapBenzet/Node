d503ed13e2346db51d1abab09d4d771c2219a550
Sedad PEHLİVANLI <sedadp@finecus.com>
2016-03-10 23:46:15 +0200

    Yorum satiri silindi.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  //index sayfasina title field i olan object gonder
   res.render('index', { title: 'nodeprogramlama.com' });
 });
 
